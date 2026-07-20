04ff8bff63ef3b0f3485ddb4ddd10894a0dbc034
szemlyansky <szemlyansky@ozon.ru>
2026-07-20 17:31:29 +0300

    disable some features

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,36 +2,7 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      opts.sections.lualine_x = {
-        Snacks.profiler.status(),
-        function()
-          return require("noice").api.status.command.get()
-        end,
-        cond = function()
-          return package.loaded["noice"] and require("noice").api.status.command.has()
-        end,
-        color = function()
-          return { fg = Snacks.util.color("Statement") }
-        end,
-        function()
-          return require("noice").api.status.mode.get()
-        end,
-        cond = function()
-          return package.loaded["noice"] and require("noice").api.status.mode.has()
-        end,
-        color = function()
-          return { fg = Snacks.util.color("Constant") }
-        end,
-        function()
-          return "  " .. require("dap").status()
-        end,
-        cond = function()
-          return package.loaded["dap"] and require("dap").status() ~= ""
-        end,
-        color = function()
-          return { fg = Snacks.util.color("Debug") }
-        end,
-      }
+      opts.sections.lualine_x = {}
       opts.sections.lualine_y = {
         { "location", padding = { left = 0, right = 1 } },
       }
