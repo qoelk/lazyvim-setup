96982ecb93f8ae2eb8915e282d0a61a3fcc2c60c
szemlyansky <szemlyansky@ozon.ru>
2026-07-20 17:04:50 +0300

    rework debugger interface

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,4 +3,24 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     enabled = false,
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = false,
+  },
+  {
+    "igorlfs/nvim-dap-view",
+    event = "VeryLazy",
+    opts = {
+      inline = false,
+    },
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dap-view").toggle()
+        end,
+        desc = "Toggle DapView",
+      },
+    },
+  },
 }
